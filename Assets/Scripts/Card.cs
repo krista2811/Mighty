eafe36de534851820f43429855a7b052f4f01515
krista2811 <krista2811@naver.com>
2018-12-24 10:33:59 +0900

    [#3] Add gitignore & enums

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+    private Sprite front;
+    private Sprite back;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public enum Suit {
+    CLOVER,
+    HEART,
+    DIAMOND,
+    SPADE,
+    NONE
+}
+
+public enum Rank {
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+}
+
+public enum Job {
+    NONE,
+    JOKER,
+    JOKER_CALL,
+    MIGHTY
+}
