888fb0d805bd16b5952384d96552cf38462bcaae
krista2811 <krista2811@naver.com>
2018-12-24 23:23:04 +0900

    [#3] Make Card Class

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -3,18 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Card : MonoBehaviour {
-    private Sprite front;
-    private Sprite back;
+    public Dictionary<string, Sprite> Images { get; set; }
+    public Sprite Front { get; set; }
+    public Sprite Back { get; set; }
+    public Suit CardSuit { get; set; }
+    public Rank CardRank { get; set; }
+    public Job CardJob { get; set; }
+    public bool IsBack { get; set; }
+    public int Point { get; set; }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    Sprite imageShow;
+
+    // Use this for initialization
+    void Start () {
+        try {
+            setImageShow(Images["back"]);
+        } catch (KeyNotFoundException e) {
+            Debug.Log(e);
+        }
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    bool Flip(bool isBack) {
+        IsBack = isBack;
+        if (IsBack) {
+            setImageShow(Images["back"]);
+        } else {
+            setImageShow(Images["front"]);
+        }
+        return IsBack;
+    }
+
+    Sprite getImage() {
+        return imageShow;
+    }
+
+    void setImageShow(Sprite image) {
+        imageShow = image;
+    }
 }
 
 public enum Suit {
