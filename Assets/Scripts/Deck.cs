fd89a8db84186a9d989c74429f782db863617a91
krista2811 <krista2811@naver.com>
2018-12-25 21:08:21 +0900

    [#4] Made Card Class

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour {
+    public Dictionary<string, Sprite> Images { get; set; }
+    public List<Card> Cards { get; set; }
+
+    private bool isShuffle;
+
+	// Use this for initialization
+	void Start () {
+        isShuffle = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private int GetDeckSize() {
+        return Cards.Count;
+    }
+
+    public void Shuffle() {
+        ShuffleList<Card>(Cards);
+    }
+
+    private void Reset()
+    {
+        // TODO: make 53 cards.
+    }
+
+    private static void ShuffleList<T>(List<T> list)
+    {
+        int random1;
+        int random2;
+
+        T tmp;
+
+        for (int index = 0; index < list.Count; ++index)
+        {
+            random1 = UnityEngine.Random.Range(0, list.Count);
+            random2 = UnityEngine.Random.Range(0, list.Count);
+
+            tmp = list[random1];
+            list[random1] = list[random2];
+            list[random2] = tmp;
+        }
+    }
+}
---
+++ Assets/Scripts/TestingScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour {
+
+    public GameObject OneCardImage;
+    public Sprite Front;
+    public Sprite Back;
+    public Sprite WhiteBack;
+
+    public bool flip;
+    public bool white;
+
+    Card cardClass;
+    Dictionary<string, Sprite> newImage = 
+        new Dictionary<string, Sprite>();
+
+	// Use this for initialization
+	void Start () {
+        newImage.Add("front", Front);
+        newImage.Add("back", Back);
+
+        cardClass = new Card
+        {
+            Images = newImage,
+            CardSuit = Suit.CLOVER,
+            CardRank = Rank.ACE,
+            CardJob = Job.NONE,
+            Point = 1,
+            IsBack = true
+        };
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cardClass.Flip(flip);
+        if (white) {
+            newImage["back"] = WhiteBack;
+        } else {
+            newImage["back"] = Back;
+        }
+        cardClass.Images = newImage;
+
+        OneCardImage.GetComponent<SpriteRenderer>().sprite 
+                    = cardClass.getImage();
+    }
+}
