ecee1cb5d59341498fc3a12ba898983195c05875
krista2811 <krista2811@naver.com>
2018-12-26 04:16:02 +0900

    [#10] Add Card class

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand {
+    List<GameObject> CardObjects;
+    bool IsBack;
+
+    public Hand(bool isBack) {
+        IsBack = isBack;
+    }
+
+    public void AddCard(GameObject OneCard) {
+        OneCard.GetComponent<CardManager>().FlipCard(IsBack);
+        CardObjects.Add(OneCard);
+    }
+
+    public int GetCardCount() {
+        return CardObjects.Count;
+    }
+}
