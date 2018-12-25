f7a22fb639e8c3ad8adeab730ba39e763d9de4f8
krista2811 <krista2811@naver.com>
2018-12-26 02:11:58 +0900

    [#4] Checked all functionalities of DeckManager & Changed Mighty & other card issues

--- Assets/Scripts/TestingScript.cs
+++ Assets/Scripts/TestingScript.cs
@@ -5,45 +5,39 @@ using UnityEngine;
 public class TestingScript : MonoBehaviour {
 
     public GameObject OneCardImage;
-    public Sprite Front;
-    public Sprite Back;
-    public Sprite WhiteBack;
 
     public bool flip;
     public bool white;
-
-    Card cardClass;
-    Dictionary<string, Sprite> newImage = 
-        new Dictionary<string, Sprite>();
-
+    
 	// Use this for initialization
 	void Start () {
-        newImage.Add("front", Front);
-        newImage.Add("back", Back);
-
-        cardClass = new Card
-        {
-            Images = newImage,
-            CardSuit = Suit.CLOVER,
-            CardRank = Rank.ACE,
-            CardJob = Job.NONE,
-            Point = 1,
-            IsBack = true
-        };
-		
-	}
+        //Debug.Log(DeckManager.GetDeckManager.CreateDeck());
+        //deck = DeckManager.GetDeckManager.CreateDeck();
+        //OneCardImage = deck.Pop();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        cardClass.Flip(flip);
-        if (white) {
-            newImage["back"] = WhiteBack;
-        } else {
-            newImage["back"] = Back;
-        }
-        cardClass.Images = newImage;
+        try {
+            OneCardImage.GetComponent<CardManager>().FlipCard(flip);
+        } catch(UnityException e) {
 
-        OneCardImage.GetComponent<SpriteRenderer>().sprite 
-                    = cardClass.GetImage();
+        }
+    }
+
+    public void GenerateDeck() {
+        DeckManager.GetDeckManager.ResetDeck();
+        OneCardImage.SetActive(false);
+        GameObject CardObject = DeckManager.GetDeckManager.Draw();
+        OneCardImage = CardObject.gameObject;
+    }
+
+    public void DrawCard() {
+        OneCardImage.SetActive(false);
+        OneCardImage = DeckManager.GetDeckManager.Draw();
+    }
+
+    public void ShuffleCard() {
+        DeckManager.GetDeckManager.ShuffleDeck();
     }
 }
